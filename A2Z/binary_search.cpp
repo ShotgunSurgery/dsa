481482419df5ea58b3e72baee9dd77e12114ee71
ShotgunSurgery <adityasmagrawal@gmail.com>
2025-07-18 04:57:31 +0000

    -

--- A2Z/binary_search.cpp
+++ A2Z/binary_search.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Solution{
-	public:
-		int search(vector<int>& nums, int target){
-			
-		}
+int search (vector<int>& v, int target){
+	int low = 0, high = (v.size() - 1), avg = 0;
+avg = (((low+high)/2) - 1);
+	while(low < high){
+		
+		if (v[avg] == target)
+            return avg;
+        else if (v[avg] < target)
+            low = avg + 1;
+        else
+            high = avg - 1;
 
+	}
+	return(low);
 };
 
 int main(){
-	int nums[7] = [1,2,3,4,5,6,7];
-	search obj;
-	obj.search(
-
+	vector<int> vm = {1,2,3,4,5,6,7};
+	cout<<"Index of the required element is: "<<search(vm, 3)<<endl;
+	return 0;
 }
