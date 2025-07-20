27ac1e6d853a29b80adf9e10bac7d4dba3c9f6cd
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-20 16:45:29 +0530

    -

--- A2Z/step4/upper_bound.cpp
+++ A2Z/step4/upper_bound.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Solution{
 	public:
 		int upperBound(vector<int> &v,int target){
-			
+			int low = 0, high = v.size() - 1, ans = 0;
+			while(low <= high){
+				int mid = (low + high)/2;
+				if(target == v[mid]){
+					ans = mid;
+					low = mid + 1;
+
+				}
+
+				else if(target > v[mid]){
+					low = mid + 1;
+				}
+
+				else{
+					high = mid - 1;
+				}
+			}
+
+			return ans; 
 		}
-}
+};
+
 int main(){
+	Solution s;
 	vector<int> v = {1,2,2,3};
-	int target;
-	cout<<"Index: "<<upperBound(v, target); 
+	int target = 2;
+	cout<<"Index: "<<s.upperBound(v, target); 
 }
 
