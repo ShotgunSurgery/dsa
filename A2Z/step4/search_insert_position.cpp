bca3009da72909383744df15396b1f8a4d60726a
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-21 05:27:33 +0530

    -

--- A2Z/step4/search_insert_position.cpp
+++ A2Z/step4/search_insert_position.cpp
@@ -5,36 +5,29 @@ using namespace std;
 class Solution{
     public:
     int searchInsert(vector<int> & nums, int target){
-        int low = 0, high = nums.size() - 1, ans = 0;
-        while(low <= high){
-            int mid = (low + high)/2;
+
+        if( nums.empty() || nums.size() == 0) return 0;
+ 
+        int low = 0, high = nums.size() - 1, n = nums.size();
+
+        while(low < high){
+
+            int mid = low + (high - low)/2;
             
-            if(low == high && low == mid){
-                ans = mid;
-                low = mid + 1;
+            if(target == nums[mid]){
+                mid;
             }
 
-            else if(nums[mid] == target){
-                return mid;
+            else if(nums[mid] > target){
+                high = mid;
             }
 
             else if(target > nums[mid] ){
                 low = mid + 1;
             }
-
-            else{
-                high = mid - 1;
-            }
         }
         
-        if(nums[ans] == target){
-            return ans;
-        }
-        else if(nums[ans] < target){
-            return (ans + 1);
-        }
-        else{
-        return (ans);}
+       return nums[low] < target ? low + 1: low;
 
     }
 };
