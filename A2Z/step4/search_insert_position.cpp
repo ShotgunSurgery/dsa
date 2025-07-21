fb4c1c433ec0dbc2d8f70c2367dbc44395aaeb54
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-21 17:01:58 +0530

    Update on 2025-07-21 17:01:58

--- A2Z/step4/search_insert_position.cpp
+++ A2Z/step4/search_insert_position.cpp
@@ -2,39 +2,45 @@
 #include <vector>
 using namespace std;
 
-class Solution{
-    public:
-    int searchInsert(vector<int> & nums, int target){
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
 
-        if( nums.empty() || nums.size() == 0) return 0;
- 
+        if (nums.empty() || nums.size() == 0)
+            return 0;
+
         int low = 0, high = nums.size() - 1, n = nums.size();
 
-        while(low < high){
+        while (low < high)
+        {
 
-            int mid = low + (high - low)/2;
-            
-            if(target == nums[mid]){
-                mid;
+            int mid = low + (high - low) / 2;
+
+            if (target == nums[mid])
+            {
+                return mid;
             }
 
-            else if(nums[mid] > target){
+            else if (nums[mid] > target)
+            {
                 high = mid;
             }
 
-            else if(target > nums[mid] ){
+            else if (target > nums[mid])
+            {
                 low = mid + 1;
             }
         }
-        
-       return nums[low] < target ? low + 1: low;
 
+        return nums[low] < target ? low + 1 : low;
     }
 };
-int main(){
+int main()
+{
     Solution s;
-    vector<int> nums = { 1,3,5,6};
+    vector<int> nums = {1, 3, 5, 6};
     int target = 7;
-    cout<<s.searchInsert(nums, target);
-
+    cout << s.searchInsert(nums, target);
 }
