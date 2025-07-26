4fbceb86e10779d64ed23b1430240d2511e90718
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-26 12:26:32 +0530

    -

--- code_chef/frequency_of_each_element_in_array.cpp
+++ code_chef/frequency_of_each_element_in_array.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-        int test_case;
-        cin>>test_case;
-        int** ptr = (int**)malloc(test_case * sizeof(int));
-        for(int i = 0; i < test_case; i++){
-            int size;
-            cin>>size;
-            ptr[i] = (int*)malloc(size * sizeof(int));
-        }
-            
+void function(const vector<int> &input_array)
+{
+    int max_elementt = *max_element(input_array.begin(), input_array.end());
+    vector<int> recorder(max_elementt + 1, 0);
 
+    for (int integer : input_array)
+    {
+        recorder[integer] += 1;
+    }
+    for (int integer : recorder)
+    {
+        cout << integer;
+    }
+}
+
+int main()
+{
+    vector<int> input_array = {1, 2, 3, 4, 5};
+    function(input_array);
+    return 0;
 }
