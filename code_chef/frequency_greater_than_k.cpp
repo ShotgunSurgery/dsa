ea6f370deb9d714178602fb94c23fa5eac72b8bc
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-30 18:13:13 +0530

    -

--- code_chef/frequency_greater_than_k.cpp
+++ code_chef/frequency_greater_than_k.cpp
@@ -6,17 +6,26 @@
 // this diffres from hash maps as elements are in order unlike hash maps (i.e. ordered maps). 
 // frequency map / frequency counter / tally map / histogram -> used to store the count of ouccerence of elements i.e. keys: values :: element: count_of_that_element
 
-
 #include <iostream>
+#include <map>
 using namespace std;
 
-void frequency_function(int N, int K, string S){
-    
-}
 int main(){
     int N, K;
     string S;
     cin>>N;
     cin>>K;
     cin>>S;
-    frequency_function(N, K, S);
+    
+    // map is cpp standard template library contianer, char is the data type of the key and int is the datatype of the 
+    // value and mp is the name of the variable 
+    map <char, int> mp;
+    for(char & ch : S){
+        mp[ch]++;
+    }
+    // auto tells the compiler to automatically deduce the datatype of the element it's iterating
+    for (auto x: mp){
+        if(x.second >= K) cout << x.first;
+    }
+    return 0;
+}
