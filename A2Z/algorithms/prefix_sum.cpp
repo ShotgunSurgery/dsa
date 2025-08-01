1137a2e4ced93a3e82b95c5e7fccafa2b4d56c7e
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-01 21:48:11 +0530

    -

--- A2Z/algorithms/prefix_sum.cpp
+++ A2Z/algorithms/prefix_sum.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
-#include <vector> 
+#include <vector>
 
 using namespace std;
 
 class NumArray {
 public:
-    NumArray(vector<int>& nums) {
-        
+    vector<int> prefix;
+
+    NumArray(vector<int> &nums) {
+        prefix.push_back(0);  // Initialize prefix[0] = 0
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefix.push_back(prefix.back() + nums[i]);
+            cout << "prefix[" << i + 1 << "] = " << prefix[i + 1] << endl;
+        }
     }
-    
+
     int sumRange(int left, int right) {
-        
+        cout << "prefix[" << right + 1 << "] = " << prefix[right + 1]
+             << ", prefix[" << left << "] = " << prefix[left] << endl;
+
+        return prefix[right + 1] - prefix[left];
     }
 };
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray* obj = new NumArray(nums);
- * int param_1 = obj->sumRange(left,right);
- */
-int main(){
-    
+int main() {
+    vector<int> nums = {-2, 0, 3, -5, 2, -1};
+    NumArray obj(nums);
 
+    cout << "sumRange(0, 2): " << obj.sumRange(0, 2) << endl;
+    cout << "sumRange(0, 5): " << obj.sumRange(0, 5) << endl;
+    cout << "sumRange(2, 5): " << obj.sumRange(2, 5) << endl;
+
+    return 0;
 }
