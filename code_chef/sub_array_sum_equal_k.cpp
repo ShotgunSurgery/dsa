32c43e7841ed89cfdb639ff3b0f572e8d9b4d871
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-01 18:29:12 +0530

    -

---
+++ code_chef/sub_array_sum_equal_k.cpp
@@ -0,0 +1,25 @@
+// prefix sum / cumulative sum / running tool -> technique                              
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution{
+    public: 
+    int subarraySum(vector<int> V, int N, int target_sum){
+        int count = 0, sum = 0;
+        unordered_map <int, int> map;
+              
+    }
+};
+
+float main(){
+    int N;
+    vector<int> V;
+    int target_sum;
+    Solution s;
+    cout << "Enter the number of elements, target sum: ";
+    cout << s.subarraySum(V, N, target_sum);
+    return 0;             
+}
