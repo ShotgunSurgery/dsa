6a66998f7ae2ace4ec478226eb6eb8ed5173e6d7
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-03 18:03:53 +0530

    -

--- code_chef/group_anagrams.cpp
+++ code_chef/group_anagrams.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 using namespace std;
 
 class Solution {
   public:
     vector<vector<string>> groupAnagrams(const vector<string>& strs){
-      
+
+      vector<unordered_map<char, int>> maps;
+      vector<vector<string>> final;
+
+      for(int j = 0; j < strs.size(); j++){
+        unordered_map<char, int> map;
+        for(char ch : strs[j]){
+            map[ch]++;
+        }
+        maps.push_back(map);
+      }
+
+      final[0].push_back(strs[0]);
+
+      for(auto k = maps.begin(); k < maps.end(); k++){
+        if(maps)
+      }
     }
 };
 
 int main(){
     int no_strings;
-    vector<string> vector;
+    vector<string> vector_strings;
     for(int i = 0; i < no_strings; i++){
         char str[10];
         cin >> str;
-        vector.push_back(str);
+        vector_strings.push_back(str);
     }
     Solution s;
-    vector<vector<string>> = s.groupAnagrams(vector);
+    vector<vector<string>> output = s.groupAnagrams(vector_strings);
 }
