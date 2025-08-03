7e96cbdc72d6ce49286b79f9522e0d8e8ae98a3f
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-03 16:36:42 +0530

    -

---
+++ code_chef/group_anagrams.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    vector<vector<string>> groupAnagrams(const vector<string>& strs){
+      
+    }
+};
+
+int main(){
+    int no_strings;
+    vector<string> vector;
+    for(int i = 0; i < no_strings; i++){
+        char str[10];
+        cin >> str;
+        vector.push_back(str);
+    }
+    Solution s;
+    vector<vector<string>> = s.groupAnagrams(vector);
+}
