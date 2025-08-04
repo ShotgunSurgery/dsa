391a870d9deb14b9ccdcf5c84a165fc9ea46b3cc
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-04 14:02:52 +0530

    -

--- dump/stack_exersise1.cpp
+++ dump/stack_exersise1.cpp
@@ -11,9 +11,17 @@ int main(){
     st.push(4);
     st.push(5);
 
-    stack<int> st_cpy(st);
-    st_cpy.pop();
-    int i = st_cpy.top();
-    st_cpy.pop();
-    cout << i << endl;
+    // stack<int> st_cpy(st);
+    // st_cpy.pop();
+    // int i = st_cpy.top();
+    // st_cpy.pop();
+    // cout << i << endl;
+
+    int n = 3;
+    for(int i = 0; i < n - 1; i++){
+        st.pop();
+    }
+    int output = st.top();
+    st.pop();
+    cout << output;
 }
