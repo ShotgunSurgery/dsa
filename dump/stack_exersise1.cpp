e489c4470eb858ff7d92c62b876eff478cd0acf5
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-04 13:59:58 +0530

    -

---
+++ dump/stack_exersise1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    stack<int> st_cpy(st);
+    st_cpy.pop();
+    int i = st_cpy.top();
+    st_cpy.pop();
+    cout << i << endl;
+}
