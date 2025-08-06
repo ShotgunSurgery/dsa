5035f24b140b2895d7f7e0516d982043bbdf92f2
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-06 15:16:37 +0530

    -

--- A2Z/algorithms/sorting_algorithms/merge_sort.c
+++ A2Z/algorithms/sorting_algorithms/merge_sort.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-merge_sort(int array[5]){
-    while(){
-           
-    }
-
+merge_sort(int array[5], int left, int right){
+    int mid = left + (right - left)/2;
+    
 }
 int main(){
     int array[5] = {29, 10, 14, 37, 13};
-    merge_sort(array);
+    int n = sizeof(array) / sizeof(array[0]);
+    merge_sort(array, 0, n - 1);
 }
