ded77a34e9e35f43de30665bd48665b4a04e041d
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-07 22:31:54 +0530

    -

--- A2Z/step4/search_in_rotated_sorted_array.cpp
+++ A2Z/step4/search_in_rotated_sorted_array.cpp
@@ -6,6 +6,20 @@ using namespace std;
 class Solution{
     public: 
     int search(vector<int> & nums, int target){
+        int low = 0, high = sizeof(nums) - 1;
+        while(low < high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] > nums[low]){
+                if(nums[mid] >= target && nums[low] <= target){
+                    high = mid;
+                    continue;
+                }
+            }
+            if(nums[mid] <  nums[high]){
+                
+            };
+        }
          
     }
 
@@ -14,7 +28,7 @@ class Solution{
 int main(){
     Solution s;
     vector<int> nums = {4, 5, 6, 7, 0, 1, 2, 3};
-    int target = 0;
+    int target = 3;
     cout << s.search(nums, target);
 }
 
