8a4ed4ad38979e12e5d42b7022ad5d24d7537e4b
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-08 17:07:30 +0530

    -

--- LeetCode/sqrtx.cpp
+++ LeetCode/sqrtx.cpp
@@ -7,7 +7,19 @@ class Solution
 public:
     int mySqrt(int x)
     {
-        int left = 0, right = 
+        int left = 0, right = x, temp;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            long long sqr = 1LL * mid * mid;
+            if(sqr == x) return mid;
+            else if(sqr > x) right = mid - 1;
+            else{
+                left = mid + 1;
+                temp = mid;
+                // cout << "left: " << left << " mid: " << mid << " right" << right << endl;
+            }
+        }
+        return temp;
     }
 };
 int main()
