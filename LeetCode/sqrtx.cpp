63f8eb1d4214bad9591285c7080072c14505710a
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-08 16:06:20 +0530

    -

---
+++ LeetCode/sqrtx.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        int left = 0, right = 
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    Solution s;
+    cout << s.mySqrt(n);
+}
