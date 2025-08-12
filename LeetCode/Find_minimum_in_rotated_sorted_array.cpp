b40efc4d4ca2e74ae28612daf499343093c5676d
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-12 08:50:29 +0530

    -

--- LeetCode/Find_minimum_in_rotated_sorted_array.cpp
+++ LeetCode/Find_minimum_in_rotated_sorted_array.cpp
@@ -3,37 +3,43 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        int lo = 0, hi = (nums.size() - 1), lowest = nums[lo];
-        while(lo <= hi){
-            int mid = lo + (hi - lo)/2;
-            // cout << "lo: " << lo << " " << " hi: " << hi << " " << " mid: " << mid << endl;
-            
-            if(nums[lo] < nums[mid]){
-                // cout << "lo: " << nums[lo] << " " << " hi: " << nums[hi] << " " << " mid: " << nums[mid] << " lowest: " << lowest << endl;
-                                //  return -1;
-                if(nums[lo] <= lowest){
-                    lowest = nums[lo];
-                    lo = mid + 1;
-                                // cout << "lo: " << lo << " " << " hi: " << hi << " " << " mid: " << mid << endl;
-                                //  return -1;  
-                };
+    class Solution {
+    public:
+        int findMin(vector<int>& nums) {
+            int lo = 0, hi = (nums.size() - 1), lowest = nums[lo];
+            while(lo <= hi){
+                int mid = lo + (hi - lo)/2;
+                // cout << "lo: " << lo << " " << " hi: " << hi << " " << " mid: " << mid << endl;
+                // return -1; 
+                if(nums[lo] <= nums[mid]){
+                    // cout << "lo: " <<  nums[lo] << " " << " hi: " << nums[hi] << " " << " mid: " << nums[mid] << " lowest: " << lowest << endl;
+                                    //   return -1;
+                    if(nums[lo] <= lowest){
+                        lowest = nums[lo];
+                        lo = mid + 1;
+                                    // cout << "lo: " << lo << " " << " hi: " << hi << " " << " mid: " << mid << endl;
+                                    //   return -1;  
+                    }
+                    else{
+                        lo = mid + 1;    
+                    }
+                }
+                else{
+                    if(nums[mid] <= lowest){
+                        lowest = nums[mid];
+                        hi = mid - 1;    
+                    }
+                    else{
+                        lo = mid + 1; 
+                    } 
+                }
             }
-            else{
-                  if(nums[mid] <= lowest){
-                    lowest = nums[mid];
-                    hi = mid - 1;
-                  };
-            }
+            return lowest;
         }
-        return lowest;
-    }
-};
+    };      
 
 int main(){
     Solution s;
-    vector<int> nums = {3, 4, 5, 0, 1, 2};
+    vector<int> nums = {2, 3, 4, 5, 1};
     cout << s.findMin(nums);    
 }
