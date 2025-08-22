902cc5efe363a3c979edfc4c688c9e8bbcf14526
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-22 18:53:43 +0530

    -

--- A2Z/ds/stack/monotonic_stack.cpp
+++ A2Z/ds/stack/monotonic_stack.cpp
@@ -1,7 +1,17 @@
 // Increasing monotonic stack -> elements are placed in increasing order
 // Decreasing monotonic stack -> elements are placed in decreasing order 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> monotonicallyIncreasing(vector<int> & nums){
+
+}
 
 int main(){
+    vector<int> nums = {3, 1, 4, 1, 5, 9, 2, 6};
+    vector<int> result = monotonicallyIncreasing(nums);
 
 }
