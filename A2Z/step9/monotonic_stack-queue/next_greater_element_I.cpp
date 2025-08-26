4b2912232b40b4e3d6031de7de6254eb26586059
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-08-26 18:29:25 +0530

    -

--- A2Z/step9/monotonic_stack-queue/next_greater_element_I.cpp
+++ A2Z/step9/monotonic_stack-queue/next_greater_element_I.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+
 using namespace std;
 
-int main(){
-    
+class Solution
+{
+public:
+    vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
+    {
+        unordered_map<int, int> nge;
+        stack<int> st;
+        vector<int> result;
+        int int_nge;
+
+        for (int integer : nums2)
+        {
+            nge[integer] = -1;
+        }
+
+        for (int x : nums2)
+        {
+            while (!st.empty() && st.top() < x)
+            {
+                nge[st.top()] = x;
+                st.pop();
+            }
+            st.push(x);
+        }
+
+        for (int i = 0; i < nums1.size(); i++)
+        {
+            result.push_back(nge[nums1[i]]);
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    vector<int> nums1 = {4, 1, 2};
+    vector<int> nums2 = {1, 3, 4, 2};
+
+    vector<int> result = s.nextGreaterElement(nums1, nums2);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
 }
